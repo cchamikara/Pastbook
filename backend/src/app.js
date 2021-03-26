e4997552ce131fda9fc7e2f242160ed171b586bb
chamal <chamalchamikara@gmail.com>
2021-03-26 23:06:38 +0530

    BE | Saving image grid

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,80 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 import config from "./config";
 
 import fetchImages from "./controllers/fetchImages";
 import calculateAspectRatio from "./controllers/calculateAspectRatio";
 
+import Grid from "./models/Grid";
+
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const api = new express.Router();
+
+mongoose
+  .connect("mongodb://mongodb:27017/backend", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 /**
  * End point for image fetching
  */
 api.get("/images", fetchImages, calculateAspectRatio, (req, res) => {
   res.status(200).json(res.locals.response);
+});
+
+/**
+ * Endpoint for get all grids
+ */
+
+api.get("/grid", async (req, res) => {
+  try {
+    const grids = await Grid.find();
+    res.json(grids);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+api.get("/grid/:fingerPrint", async (req, res) => {
+  try {
+    const { fingerPrint } = req.params;
+
+    const grid = await Grid.find({ fingerPrint });
+
+    if (grid == null) {
+      res.status(404).json({ message: "Cannot find grid" });
+    } else {
+      res.json(grid);
+    }
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+/**
+ * Endpoint for saving selected grid
+ */
+
+api.post("/grid", async (req, res) => {
+  const { fingerPrint, imageGrid } = req.body;
+
+  const grid = new Grid({
+    fingerPrint,
+    imageGrid,
+  });
+  try {
+    const newGrid = await grid.save();
+    res.status(201).json(newGrid);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 /**
